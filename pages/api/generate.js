957bd28c0a598c85bfd7bbdbb09c41529b88ad8f
dosvidosikml-netizen <dosvidosikml@gmail.com>
2026-03-26 00:51:56 +0200

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
     }
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash-001:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash-8b:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: {
